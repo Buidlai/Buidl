6c5161a923429ad502ea57c4d47a6df266bae6c1
eminivince <communicatti@gmail.com>
2024-05-14 15:11:32 +0100

    updated sale address

--- src/contracts/launchpad.js
+++ src/contracts/launchpad.js
@@ -1,4 +1,4 @@
 import ABI from "./ICO.abi.json";
-export const ICOAddress = "0x77FaAb123e19Ba32A85dcE1290f993B76F031632";
+export const ICOAddress = "0x13d7Eb9801b4e5828221308c856c9Ca8834458A0";
 export const ICO_ABI = ABI;
 export const ICO_Price = 1200;
