dbf07732b74f1d67ad3d8e19dd2983a0f60bf13d
toyman640 <falakoadebayor@gmail.com>
2024-06-04 11:30:56 +0100

    User can Sign up

--- src/pages/auth/Registration.js
+++ src/pages/auth/Registration.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import StickyNavbar from "../../components/StickyNavbar";
 import { Container, Form, Col, Row, Button } from "react-bootstrap";
+import { getUserStatus, createUser } from '../../redux/user/userSlice';
 
 import '../../../src/App.css';
 
@@ -10,7 +13,16 @@ import { Link } from 'react-router-dom';
 
 function Registration() {
   const [phone, setPhone] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userStatus, setUserStatus] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isInputFocused, setInputFocused] = useState(false);
+  const userStats = useSelector((state) => state.userStatus.userStatus.length > 0);
 
   const handleCountrySelect = () => {
     if (!isInputFocused) {
@@ -18,10 +30,50 @@ function Registration() {
     }
   };
 
+  // console.log('hello');
+
+  const handleUserStatusChange = (e) => {
+    setUserStatus(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!userStats) {
+      dispatch(getUserStatus());
+    }
+  },[dispatch, userStats]);
+  // dispatch(getUserStatus());
+
   const [isChecked, setIsChecked] = useState(false);
 
   const handleCheckboxChange = (e) => {
     setIsChecked(e.target.checked);
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[!@#$%^&*])(?=.{8,})/;
+    return regex.test(password);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can send the form data to the backend
+    if (!validatePassword(password)) {
+      alert('Password must contain at least 8 characters, 1 lowercase character, and 1 special character.');
+      return;
+    }
+    const formData = {
+      username: username,
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      phone_number: phone,
+      user_status: userStatus,
+      password: password
+    };
+    console.log('Ran');
+    dispatch(createUser(formData));
+    navigate('/verification');
+
   };
 
   const Sectionstyle = {
@@ -48,6 +100,12 @@ function Registration() {
     marginTop: '1.2rem',
   }
 
+  const uStatus = useSelector((state) => state.userStatus.userStatus);
+  // console.log(uStatus);
+
+  
+  // console.log(uStatus);
+
   return (
     
     <Container fluid className='App' style={{backgroundColor:'#272A38'}}>
@@ -61,19 +119,19 @@ function Registration() {
             Register on Buidl to enjoy the revolutionizing collaboration space
           </p>
 
-          <Form>
+        <Form>
 
       <Row>
         <Col xs={12} sm={12} md={6} lg={6}>
         <Form.Group style={formdiv}>
           <Form.Label style={inputheader}>First Name</Form.Label>
-          <Form.Control style={inputbody} placeholder="First name" />
+          <Form.Control style={inputbody} placeholder="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)}  />
           </Form.Group>
         </Col>
         <Col xs={12} sm={12} md={6} lg={6}>
         <Form.Group style={formdiv}>
           <Form.Label style={inputheader}>Last Name</Form.Label>
-          <Form.Control style={inputbody} placeholder="Last name" />
+          <Form.Control style={inputbody} placeholder="Last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
           </Form.Group>
         </Col>
       </Row>
@@ -81,7 +139,7 @@ function Registration() {
       <Row>
         <Form.Group style={formdiv}>
           <Form.Label style={inputheader}>Email Address</Form.Label>
-          <Form.Control style={inputbody} placeholder="Email address" />
+          <Form.Control style={inputbody} placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
           </Form.Group>
       </Row>
 
@@ -103,14 +161,26 @@ function Registration() {
       <Row>
         <Form.Group style={formdiv}>
           <Form.Label style={inputheader}>Username</Form.Label>
-          <Form.Control style={inputbody} placeholder="Enter Username" />
+          <Form.Control style={inputbody} placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} />
           </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group style={formdiv}>
+          <Form.Label style={inputheader}>User Status</Form.Label>
+          <Form.Select aria-label="Default select example"  value={userStatus} onChange={handleUserStatusChange}>
+            {uStatus.map((status) => (
+              <option key={status.id} value={status.id}>
+                {status.status_name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
       </Row>
 
       <Row>
         <Form.Group style={formdiv}>
           <Form.Label style={inputheader}>Password</Form.Label>
-          <Form.Control style={inputbody} placeholder="Set Password" />
+          <Form.Control style={inputbody} type="password" placeholder="Set Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </Form.Group>
           
           <ul style={{fontSize:0.8+'rem',color:'white',marginTop:1.0+"rem", marginLeft:1+'rem'}}>
@@ -149,9 +219,7 @@ function Registration() {
       />
       </Row><br/>
       
-      <Link to='./verification'>
-      <Button  style={{width:100+'%',backgroundColor:'#EEA20E',borderColor:'#EEA20E', color:'#222532',fontsize:0.8+'rem',marginBottom:1+'rem'}}>Proceed</Button>
-      </Link>
+      <Button type="button" onClick={handleSubmit} style={{width:100+'%',backgroundColor:'#EEA20E',borderColor:'#EEA20E', color:'#222532',fontsize:0.8+'rem',marginBottom:1+'rem'}}>Proceed</Button>
     <br/>
     <span style={{color:'#ffffff',fontSize:0.8+'rem'}}>Already have an Account ? 
     <Link to= './login'>
--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,38 +1,79 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const GET_USER_STATUS = "http://127.0.0.1:8000/user-status"
+const GET_USER_STATUS = "http://127.0.0.1:8000/user-status/";
+const SIGN_UP_USER = "http://127.0.0.1:8000/signup/";
 
-const initailState = {
+const initialState = {
   loading: false,
   currentUser: null,
-  userStatus: null,
+  userStatus: [],
   error: null
 }
 
 
-
-
-export const getUserStatus =  createAsyncThunk('status/getUserStatus', async (thunkAPI) => {
-  try {
-    const response = await axios(GET_USER_STATUS);
+export const getUserStatus = createAsyncThunk(
+  'userStatus/getUserStatus',
+  async (thunkAPI) => {
+    const response = await axios.get(GET_USER_STATUS);
     return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue('cannot get status!');
   }
-});
+);
 
-const UserSlice = createSlice({
-  name: 'user',
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async (formData, thunkAPI) => {
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+    try {
+      const response = await axios.post(SIGN_UP_USER, formData, {
+        headers,
+      });
+      return response.data; // Assuming your API returns data upon successful user creation
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data); // Handle error and provide rejected value
+    }
+  }
+);
+
+
+// export const getUserStatus = createAsyncThunk('status/getUserStatus', async (_, thunkAPI) => {
+//   try {
+//     const response = await axios.get(GET_USER_STATUS);
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue('cannot get status!');
+//   }
+// });
+
+const userSlice = createSlice({
+  name: 'userStatus',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(createUser.pending, (state) => ({
+        ...state,
+        loading: true,
+        error: null,
+      }))
+      .addCase(createUser.fulfilled, (state, action) => ({
+        ...state,
+        currentUser: action.payload,
+        error: null,
+      }))
+      .addCase(createUser.rejected, (state, action) => ({
+        ...state,
+        loading: false,
+        error: action.error.message,
+      }))
       .addCase(getUserStatus.fulfilled, (state, action) => ({
         ...state,
-        userStatus: action.payload
+        userStatus: action.payload,
       }))
   }
 });
 
-export default UserSlice.reducer;
+export default userSlice.reducer;
